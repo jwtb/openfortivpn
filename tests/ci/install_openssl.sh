9cd680df728cab56f33bbae2bdad683242ce8365
global-git-windows-jtb-no-H <jtb@git-windows-jtb-no-H>
2021-03-05 22:47:04 +0100

    jwtb - test2

--- tests/ci/install_openssl.sh
+++ tests/ci/install_openssl.sh
@@ -9,9 +9,11 @@ PREFIX="$1"
 VERSION=1.0.2u
 SRC="https://www.openssl.org/source/old/1.0.2/openssl-${VERSION}.tar.gz"
 
+echo "jwtb get openssl ${SRC}"
 wget -O openssl.tar.gz "$SRC"
 tar -xf openssl.tar.gz -C "$HOME"
 cd "${HOME}/openssl-${VERSION}"
+echo "jwtb - ${HOME}
 ./config --prefix="$PREFIX" shared -fPIC
 make
 make install
